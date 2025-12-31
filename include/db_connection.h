ddd7c29a0ff17d779068e1d956dcb53a1b17d448
Nisa Askar <nisa.askar@stu.fbu.edu.tr>
2025-12-31 17:26:35 +0300

    docs: add comments and interface for DB connection (Week 3)

--- include/db_connection.h
+++ include/db_connection.h
@@ -1,8 +1,20 @@
-#pragma once
-bool connect_to_database();
 #ifndef DB_CONNECTION_H
 #define DB_CONNECTION_H
 
-void connect_to_database();
+/**
+ * @file db_connection.h
+ * @brief Database connection interface for Week 3.
+ *
+ * This module prepares the database connection layer.
+ * Actual database logic will be implemented in later weeks.
+ */
 
-#endif
+class DBConnection {
+public:
+    DBConnection();
+    ~DBConnection();
+
+    void connect();
+};
+
+#endif // DB_CONNECTION_H
